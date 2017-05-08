6daf3fe0ed32e9ba5daebf7cc5498bf37411ff29
Huub Hermsen <huub@wazzurb.com>
2017-05-08 17:28:01 +0200

    Added default styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
+import "./default.scss";
+
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app.routing";
+
+import { NotLazyModule } from "@app/modules";
 
 @NgModule({
     imports: [
       BrowserModule,
-      AppRoutingModule
+      AppRoutingModule,
+      NotLazyModule
     ],
     declarations: [
       AppComponent
